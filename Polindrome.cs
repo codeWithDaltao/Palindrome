bcb2c57ff976f5083019633ad75a91ee03e9e6e9
daltonfontes <fontes.dalton@hotmail.com>
2024-09-11 20:25:02 -0300

    Add project files.

---
+++ Polindrome.cs
@@ -0,0 +1,24 @@
+﻿namespace Palindrome
+{
+    public class Polindrome
+    {
+        public static bool FirstQueue(string input)
+        {
+            if (input.Length > 0) return true;
+            return false;
+        }
+
+        public string SecondQueue(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        public bool LoopToCompare(string input, string reverseString)
+        {
+            if (input == reverseString) return true;
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Palindrome;
+
+Polindrome polindrome = new();
+Console.WriteLine("enter words");
+string input = Console.ReadLine();
+string reversed = polindrome.SecondQueue(input);
+
+if (polindrome.LoopToCompare(input,reversed))
+{
+    Console.WriteLine("is a polindrome");
+}
+
+else
+{
+    Console.WriteLine("not a polindrome");
+}
